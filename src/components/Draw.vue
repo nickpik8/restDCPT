e2c7d62ab27ffa69a256ce084baf851d54e917c3
Nick <rekdirect8@yandex.ru>
2020-11-06 11:13:44 +0300

    новая ветка canvas

--- src/components/Draw.vue
+++ src/components/Draw.vue
@@ -4,14 +4,23 @@
 </template>
 <script>
 export default {
-    data: function(){
+    data(){
         return {
-            range: []
+            range: {
+                x: {
+                    begin: [],
+                    end: []
+                },
+                y: {
+                    begin: [],
+                    end: []
+                } 
+            }
         }
     },
     methods : {
         draw: function(){ 
-            let ctx = this.$refs.canvas.getContext('2d');
+           /* let ctx = this.$refs.canvas.getContext('2d');
             let w = this.$refs.canvas.width;
             let h = this.$refs.canvas.height;
             ctx.fillStyle = "gray";
@@ -34,9 +43,27 @@ export default {
             ctx.strokeRect(bx + 176, y - by - 100, 100, 200);
             ctx.strokeRect(bx + 298, y - by - 100, 150, 200);
             ctx.strokeRect(bx + 503, y - by - 100, 150, 200);
+            this.range.x.begin.push(bx);
+            this.range.x.end.push(bx + 100);
+            this.range.x.begin.push(bx + 176);
+            this.range.x.end.push(bx + 176 + 100);
+            this.range.x.begin.push(bx + 298);
+            this.range.x.end.push(bx + 298 + 150);
+            this.range.y.begin.push(y - by - 100);
+            this.range.y.end.push(y - by);
+            this.range.y.begin.push(y - by - 100);
+            this.range.y.end.push(y - by);*/
         },
         clickCanvas(event){
-            
+         /*   let ctx = this.$refs.canvas.getContext('2d');
+            let x = event.x;
+            let y = event.y;
+            for (let i = 0; i < 2; i++){
+                if (x > this.range.x.begin[i] && x < this.range.x.end[i] && y > this.range.y.begin[i] && y < this.range.y.end[i]){
+                    ctx.fillStyle = "green";
+                    ctx.fillRect(this.range.x.begin[i], this.range.y.begin[i], this.range.x.end[i] - this.range.x.begin[i], this.range.y.end[i] - this.range.y.begin[i]);
+                }
+            }*/
         }
     },
     computed:{
